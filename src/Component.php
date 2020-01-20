9863ee17a964b82f475eb87a3e737f9838e48548
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-20 17:48:26 +0200

    added component file

---
+++ src/Component.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Azonmedia\UrlRewriting;
+
+use Azonmedia\Components\BaseComponent;
+
+class Component extends BaseComponent
+{
+
+    protected const COMPONENT_NAME = "Url Rewriting package";
+    protected const COMPONENT_URL = 'https://url-rewriting.packages.guzaba.org/';
+    protected const COMPONENT_NAMESPACE = __NAMESPACE__;
+    protected const COMPONENT_VERSION = '0.0.1';
+    protected const VENDOR_NAME = 'Azonmedia';
+    protected const VENDOR_URL = 'https://azonmedia.com';
+    protected const ERROR_REFERENCE_URL = 'https://github.com/AzonMedia/url-rewriting/docs/ErrorReference/';
+}
